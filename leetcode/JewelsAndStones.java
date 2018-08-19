2eacef2c08cb0fd708030eb1b8d1fd19421792b4
YaHeng Su <yaheng.su1@gmail.com>
2018-08-19 16:08:49 -0400

    commit

---
+++ leetcode/JewelsAndStones.java
@@ -0,0 +1,15 @@
+package leetcode;
+
+class Solution {
+    public int numJewelsInStones(String J, String S) {
+        int count = 0;
+        char[] a = J.toCharArray();
+        for(int i = 0; i < S.length(); i++){
+            for(int j = 0; j < a.length; j++){
+                if(a[j] == S.charAt(i))
+                    count++;
+            }
+        }
+        return count;
+    }
+}
