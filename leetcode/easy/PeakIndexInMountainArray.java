c2896982fa85454bb29be4b965ed2a59a6d80cc2
YaHeng Su <yaheng.su@uwaterloo.ca>
2018-08-21 10:59:35 -0400

    some problems

---
+++ leetcode/easy/PeakIndexInMountainArray.java
@@ -0,0 +1,9 @@
+class PeakIndexInMountainArray {
+    public int peakIndexInMountainArray(int[] A) {
+        for(int i = 0; i < A.length - 1; i++) {
+            if(A[i] > A[i+1])
+                return i;
+        }
+        return 0;
+    }
+}
