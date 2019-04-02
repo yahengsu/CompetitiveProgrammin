0987130dc5e547374ed68443bec75734bffde7e3
YaHeng Su <yaheng.su1@gmail.com>
2019-04-01 20:05:35 -0400

    problems

---
+++ ctci/ArrayAndStrings/PalindromePermutation.java
@@ -0,0 +1,14 @@
+class PalindromePermutation {
+    public boolean isPalindrome(String a) {
+        boolean one = false;
+
+        //assume ascii character set (128 possible characters)
+        int[] count = new int[128];
+        //to be palindrome need the same number of characters
+        char[] chars = a.toCharArray();
+        
+
+
+        return true;
+    }
+}
---
+++ leetcode/medium/ProductOfArrayExceptSelf.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int[] productExceptSelf(int[] nums) {
+        int product = nums[0];
+        for(int i = 1; i < nums.length; i++) {
+            product *= nums[i];
+        }
+        int[] a = new int[nums.length];
+        
+        for(int i = 0; i < a.length; i++){ 
+            
+        }
+        
+        return a;
+    }
+}
