0dc53db970985fc63bc615a58fda48a00a9fdde8
YaHeng Su <yaheng.su1@gmail.com>
2020-07-28 12:37:03 -0600

    palindrome permutation and test case

--- ctci/ArrayAndStrings/PalindromePermutation.java
+++ ctci/ArrayAndStrings/PalindromePermutation.java
@@ -1,7 +1,8 @@
 class PalindromePermutation {
-    public boolean isPalindrome(String a) {
-        boolean one = false;
+    public static boolean isPalindrome(String a) {
 
+        a = a.replaceAll("\\s", "");
+        a = a.toLowerCase();
         //assume ascii character set (128 possible characters)
         int[] count = new int[128];
         //to be palindrome need the same number of characters
@@ -12,6 +13,24 @@ class PalindromePermutation {
         }
 
 
+        boolean isOddSeen = false;
+        for (int i = 0; i < count.length; i++) {
+            if (count[i] % 2 == 1) {
+                if (!isOddSeen && a.length() % 2 == 1) 
+                    isOddSeen = true;
+                else
+                    return false;
+            }
+        }
+
         return true;
+    }
+
+    public static void main(String[] args) {
+        String s1 = "Tact Coa";
+        String s2 = "not palindrome";
+
+        System.out.println(isPalindrome(s1)); // true
+        System.out.println(isPalindrome(s2)); // false
     }
 }
