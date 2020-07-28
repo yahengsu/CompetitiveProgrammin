bede4bab89d01408c4bd1a62626f2b29e28fc97c
YaHeng Su <yaheng.su1@gmail.com>
2020-07-28 12:21:09 -0600

    urlify in place and added test case

--- ctci/ArrayAndStrings/URLify.java
+++ ctci/ArrayAndStrings/URLify.java
@@ -1,10 +1,25 @@
 class URLify {
-    public static String urlify(String s) {
-        String a = s.replaceAll(" ", "%20");
-        return a;
+    public static String urlify(String s, int len) {
+        char[] a = new char[s.length()];
+        
+        int p = s.length() - 1;
+        for (int i = len - 1; i >=0; i--) {
+            if (s.charAt(i) == ' ') {
+                a[p] = '0';
+                a[p-1] = '2';
+                a[p-2] = '%';
+                p -= 3;
+            } else {
+                a[p] = s.charAt(i);
+                --p;
+            }
+        }
+        return new String(a);
     }
 
     public static void main(String[] args) {
-        System.out.println(urlify("HELLO WORLD"));
+        String toUrl = "Mr John Smith    ";
+        int toUrlLen = 13;
+        System.out.println(urlify(toUrl, toUrlLen));
     }
 }
