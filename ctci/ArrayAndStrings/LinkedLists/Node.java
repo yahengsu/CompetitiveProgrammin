42018f06b4fd724f05a865973c1cb42c379c47f7
YaHeng Su <yaheng.su1@gmail.com>
2020-07-28 21:18:56 -0600

    added linkedlist node class and remove duplicates

---
+++ ctci/ArrayAndStrings/LinkedLists/Node.java
@@ -0,0 +1,10 @@
+package LinkedLists;
+
+public class Node {
+    public Node next = null;
+    public int data;
+
+    public Node(int d) {
+        data = d;
+    }
+}
---
+++ ctci/ArrayAndStrings/LinkedLists/RemoveDuplicates.java
@@ -0,0 +1,42 @@
+package LinkedLists;
+import java.util.HashSet;
+
+import LinkedLists.Node;
+import java.util.*;
+
+public class RemoveDuplicates {
+    public static void removeDuplicates(Node node) {
+        Set<Integer> dups = new HashSet<>();
+
+        Node iter = node;
+        Node prev = node;
+        while (iter != null) {
+            int val = iter.data;
+            if (dups.contains(val)) {
+                prev.next = iter.next;
+                iter = iter.next;
+            } else {
+                dups.add(val);
+                prev = iter;
+                iter = iter.next;
+            }
+           
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] data = { 2, 3, 1, 2, 3, 2, 2, 2, 2};
+        Node n = new Node(1); // 1 2 3 1 2 3 -> 1 2 3
+        Node iter = n;
+        for(int i : data) {
+            iter.next = new Node(i);
+            iter = iter.next;
+        }
+        removeDuplicates(n);
+        iter = n;
+        while (iter != null) {
+            System.out.println(iter.data);
+            iter = iter.next;
+        }
+    }
+}
