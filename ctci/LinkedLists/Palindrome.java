c86b040fbc990b10b6f56f112ce16013b3eaf872
YaHeng Su <yaheng.su1@gmail.com>
2020-07-30 15:26:13 -0600

    add palindrome linkedlist

--- ctci/LinkedLists/Palindrome.java
+++ ctci/LinkedLists/Palindrome.java
@@ -1,12 +1,31 @@
-import java.util.HashMap;
-
-import java.util.HashMap;
-
+import java.util.*;
 public class Palindrome {
     public static boolean isPalindrome (Node n) {
-        HashMap<String> letters = new HashMap<>();
+        if (n == null) {
+            return false;
+        }
+        Deque<String> s = new ArrayDeque<>();
+        s.add(n.sData);
+        Node prev = n;
+        Node curr = n.next;
+        Node next = curr.next;
+        while(curr.next != null) {
+            s.add(curr.sData);
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        curr.next = prev;
+        s.add(curr.sData);
 
-        
+        while (s.size() > 0 && curr != null) {
+            if (s.pop() != curr.sData) {
+                return false;
+            }
+            curr = curr.next;
+        }
+        return true;
     }
     public static void main(String[] args) {
         String[] not = {"a", "l", "i", "n", "d", "r", "o", "m", "e"};
@@ -18,13 +37,13 @@ public class Palindrome {
             iter = iter.next;
         }
         System.out.println(isPalindrome(n));
-        n = new Node("p");
-        iter = n;
+        Node b = new Node("p");
+        iter = b;
         for(String i :  is) {
             iter.next = new Node(i);
             iter = iter.next;
         }
-       System.out.println(isPalindrome(n));
+       System.out.println(isPalindrome(b));
 
        
     }
