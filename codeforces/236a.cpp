9d29c163bfd44151ac9d1da77f35d09a75140b47
YaHeng Su <yaheng.su1@gmail.com>
2020-09-02 22:45:01 -0600

    236a

---
+++ codeforces/236a.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+
+
+using namespace std;
+
+int main() {
+    string s;
+    cin >> s;
+
+    unordered_set<char> a;
+
+    for (auto c : s) {
+        a.insert(c);
+    }
+
+    if (a.size() % 2 == 0) {
+        cout << "CHAT WITH HER!" << endl;
+    } else {
+        cout << "IGNORE HIM!" << endl;
+    }
+}
